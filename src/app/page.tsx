2b0f23daf73e395af26986d7a0e63a7437d34c6c
IsabelErlandsson <isabelle.erlandssons@gmail.com>
2024-04-30 23:51:14 +0200

    button updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,26 @@ import Title from "../components/Title/page";
 import styles from "./page.module.css";
 import Link from "next/link";
 import React from "react";
+import Button from "@/components/Button/page";
 
 export default function App() {
   return (
     <>
       <header className={styles.header}>
         <Link href={"/login"} className={styles.login}>
-          Logga in{" "}
+          <Button>Logga in</Button>
         </Link>
         <Link href={"/create-account"} className={styles.login}>
-          Skapa konto{" "}
+          <Button>Skapa konto</Button>
         </Link>
       </header>
       <main className={styles.main}>
         <Title />
         <div className={styles.circle}></div>
         <div className={styles.circleTwo}></div>
+        <Link className={styles.link} href={"/note"}>
+          <Button>Kom igång</Button>
+        </Link>
       </main>
     </>
   );
