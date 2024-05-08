39b1b13d66f06ad4d8afe6c0cec5ae7071e75c7c
IsabelErlandsson <isabelle.erlandssons@gmail.com>
2024-05-08 15:15:17 +0200

    fix

--- src/components/EditBar/page.tsx
+++ src/components/EditBar/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { IoColorPaletteOutline as Color } from "react-icons/io5";
 import { PiGridFourLight as Grid } from "react-icons/pi";
 
-import styles from "./edit-bar.module.css";
-
 type EditBarProps = {
   children?: string;
   type?: string;
@@ -11,7 +9,7 @@ type EditBarProps = {
 
 const EditBar: React.FC<EditBarProps> = ({ children }) => {
   return (
-    <div className={styles.editIcons}>
+    <div>
       <Color />
       <Grid />
     </div>
