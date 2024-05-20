2adba95fe37bf53586da946cd883fe7c4e575cc5
IsabelErlandsson <isabelle.erlandssons@gmail.com>
2024-05-20 16:00:01 +0200

    Checkicons

--- src/components/EditBar/page.tsx
+++ src/components/EditBar/page.tsx
@@ -4,18 +4,30 @@ import { BsLink45Deg as Link } from "react-icons/bs";
 import { PiGridFourLight as Grid } from "react-icons/pi";
 import { PiListChecksLight as Check } from "react-icons/pi";
 import { PiImages as Upload } from "react-icons/pi";
+import { AiOutlineCheckCircle as Checking } from "react-icons/ai";
+import { AiFillCheckCircle as Checked } from "react-icons/ai";
 
 import styles from "./editbar.module.css";
 
-const getCSSVariable = (variableName: string) =>
+const getCSSVariable = (variableName) =>
   getComputedStyle(document.documentElement).getPropertyValue(variableName);
 
 const EditBar = ({ onColorChange, noteId }) => {
   const [selectedColor, setSelectedColor] = useState();
+  const [showChecklist, setShowChecklist] = useState(false);
+  const [isChecklistChecked, setIsChecklistChecked] = useState(false);
 
   const changeColor = (color) => {
     setSelectedColor(color);
     onColorChange(noteId, color);
+  };
+
+  const toggleChecklist = () => {
+    setShowChecklist((prevShowChecklist) => !prevShowChecklist);
+  };
+
+  const toggleChecklistColor = () => {
+    setIsChecklistChecked((prevIsChecklistChecked) => !prevIsChecklistChecked);
   };
 
   const colorOptions = [
@@ -55,7 +67,32 @@ const EditBar = ({ onColorChange, noteId }) => {
         </div>
       </div>
       <Grid className={styles.icons} title="Lägg till en tabell" />
-      <Check className={styles.icons} title="Gör en checklista" />
+
+      <>
+        <Check
+          className={styles.icons}
+          title="Gör en checklista"
+          onClick={toggleChecklist}
+        />
+        {showChecklist && (
+          <>
+            {isChecklistChecked ? (
+              <Checked
+                className={styles.icons}
+                title="Ny checklista"
+                onClick={toggleChecklistColor}
+              />
+            ) : (
+              <Checking
+                className={styles.icons}
+                title="Ny checklista"
+                onClick={toggleChecklistColor}
+              />
+            )}
+          </>
+        )}
+      </>
+
       <Link className={styles.icons} title="Lägg till en länk" />
       <Upload className={styles.icons} title="Infoga bilaga" />
     </div>
